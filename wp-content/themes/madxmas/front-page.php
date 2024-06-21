91fd9fcf21447b05d1c3122939de2201fdb2dd7e
Manuel Novoa <novoamanuel@ymail.com>
2024-06-20 19:14:47 -0500

    Added ACF support for Single services pages.

--- wp-content/themes/madxmas/front-page.php
+++ wp-content/themes/madxmas/front-page.php
@@ -89,7 +89,7 @@ $hero_image5 = $hero_images['hero_5'];
     <!-- Right -->
     <div class="half-width flex wrap center align youtube-container mobile-video">
         <!-- Video -->
-        <iframe class="video-wrap" width="90%" height="600px" src="https://www.youtube.com/embed/zjG71j8uhc4?controls=0&autoplay=1&mute=1&loop=1&playlist=zjG71j8uhc4&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <iframe class="video-wrap" width="90%" height="600px" src="https://www.youtube.com/embed/g-5B12wpx8o?controls=0&autoplay=1&mute=1&loop=1&playlist=g-5B12wpx8o&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 </div>
 
