b292fdb8fa9cb4e6d6c7bcde718073b3044774e6
Manuel Novoa <novoamanuel@ymail.com>
2024-06-26 20:43:17 -0500

    Removed the last service in the services page.

--- wp-content/themes/madxmas/services.php
+++ wp-content/themes/madxmas/services.php
@@ -48,7 +48,7 @@ $weekend = $hours['weekend'];
 <div class="opacity-screen"></div>
     <div class="absolute title-position-hero">
         <h2 class="hero-title veneer">Customize Your Ride, Unleash Your Style!</h2>
-        <h2 class="hero-subtitle americane white-copy">Crafting Your Dream Ride,</br>One Upgrade at a Time!</h2>
+        <!-- <h2 class="hero-subtitle americane white-copy">Crafting Your Dream Ride,</br>One Upgrade at a Time!</h2> -->
     </div>
     <!-- <img class="christmas-break" src="<?php echo get_template_directory_uri();?>/src/images/curve-bg-white.png" /> -->
 </div>
